a4464a66adb08435ecaae8634284f8df15d6f4b4
Unero <20809454+un-ro@users.noreply.github.com>
2021-11-08 11:36:58 +0700

    Day 1

---
+++ day-1-to-10/day-1.py
@@ -0,0 +1,7 @@
+# Day 1: Band Name Generator
+# Learn print(), input(), and string formatting
+
+print("Welcome to the Band Name Generator!")
+city = input("What\'s name of the city you grew up in?\n")
+pet = input("What\'s your pet\'s name?\n")
+print("Your band name could be " + city + " " + pet + "!")
